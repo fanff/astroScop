9a1c85d36b8b34f0e9bdf8d6cb3203e335f7599b
fanf <fanf@fanf>
2021-03-27 19:59:31 +0100

    adding monitoring

---
+++ webapp/wasp/src/components/cameraStats.vue
@@ -0,0 +1,45 @@
+<template>
+  <div >
+Buffer Size = {{lastStat().imgbuffcount}}
+  </div>
+</template>
+
+<script>
+//import Vue from "vue"
+
+export default {
+  components: {
+  },
+
+  name: 'camStats',
+  props: {
+      camStats:Array
+  },
+  data () {
+    return {
+
+    }
+  },
+  watch:{
+  },
+  methods: {
+      lastStat: function(){
+        if(this.camStats.length>0){
+            return this.camStats[this.camStats.length-1].data;
+        }else{
+            return {}
+        }
+      },
+  },
+  mounted(){
+  },
+  beforeDestroy(){
+    //this.client.end()
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ webapp/wasp/src/components/memoryStats.vue
@@ -0,0 +1,38 @@
+<template>
+  <div >
+        {{memStats}}
+  </div>
+</template>
+
+<script>
+//import Vue from "vue"
+
+export default {
+  components: {
+  },
+
+  name: 'memstats',
+  props: {
+      memStats:Array
+  },
+  data () {
+    return {
+
+    }
+  },
+  watch:{
+  },
+  methods: {
+  },
+  mounted(){
+  },
+  beforeDestroy(){
+    //this.client.end()
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
