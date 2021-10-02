4b1224bad2203fdaf582800232c6d152e3eb6117
fanf <fanf@fanf>
2021-10-02 15:03:34 +0200

    j

--- backapp/aptinst.sh
+++ backapp/aptinst.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-apt install -y python3-numpy python3-websockets python3-pilkit python3-opencv libopenblas-dev libopenblas-base python3-picamera python3-websocket python3-serial
+apt install -y python3-psutil python3-numpy python3-websockets python3-pilkit python3-opencv libopenblas-dev libopenblas-base python3-picamera python3-websocket python3-serial
